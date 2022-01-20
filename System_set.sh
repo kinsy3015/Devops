44587ba0c943242476576947927adc13fbbea023
Sleepfuler <62363841+kinsy3015@users.noreply.github.com>
2022-01-20 20:21:51 +0900

    Update System_set.sh

--- System_set.sh
+++ System_set.sh
@@ -5,5 +5,4 @@ sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install vim tmux cmake make gcc ibus-hangul build-essential docker.io -y
 sudo apt autoremove -y
 
-
-
+sudo echo -e "set number \ncolo darkblue \nsyntax on" >> ~/.vimrc
