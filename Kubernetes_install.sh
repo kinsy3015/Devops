00fcbdee62464063f125581a63dd1c435f47ca8e
Sleepfuler <62363841+kinsy3015@users.noreply.github.com>
2022-01-20 21:10:57 +0900

    Update Kubernetes_install.sh

--- Kubernetes_install.sh
+++ Kubernetes_install.sh
@@ -7,6 +7,6 @@ sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://pack
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update -y
-sudo apt-get install -y kubelet kubeadm kubectl -y
-sudo apt-mark hold kubelet kubeadm kubectl -y
+sudo apt-get install -y kubelet kubeadm kubectl 
+sudo apt-mark hold kubelet kubeadm kubectl 
 
