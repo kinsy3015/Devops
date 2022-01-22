e2bc17f835e9d3dca47a50a5439f44f96a0aa1c7
Sunghan Kim <kinsy3015@naver.com>
2022-01-23 05:10:48 +0900

    update sh

---
+++ Hadoop_install.sh
@@ -0,0 +1,4 @@
+wget https://dlcdn.apache.org/hadoop/common/hadoop-3.3.1/hadoop-3.3.1-src.tar.gz
+wget https://dlcdn.apache.org/hadoop/common/hadoop-3.3.1/hadoop-3.3.1.tar.gz
+tar xf hadoop-3.3.1-src.tar.gz 
+
