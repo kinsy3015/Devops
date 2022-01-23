adc08467a60c9197f54918e81eb5ad694385a2a8
Sleepfuler <62363841+kinsy3015@users.noreply.github.com>
2022-01-23 12:45:32 +0900

    Update System_set.sh

--- System_set.sh
+++ System_set.sh
@@ -6,3 +6,5 @@ sudo apt-get install vim tmux cmake make gcc ibus-hangul build-essential docker.
 sudo apt autoremove -y
 
 sudo echo -e "set number \ncolo darkblue \nsyntax on" >> ~/.vimrc
+
+ibus restart
