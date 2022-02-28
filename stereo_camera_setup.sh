8f26d045c644465b9e0cffde9266f0c052e85ee9
Sleepfuler <62363841+kinsy3015@users.noreply.github.com>
2022-02-28 16:58:03 +0900

    Update stereo_camera_setup.sh

--- stereo_camera_setup.sh
+++ stereo_camera_setup.sh
@@ -2,6 +2,8 @@
 
 sudo pip install v4l2
 
+sudo apt-get install gstreamer1.0-tools libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-good1.0-dev -y
+
 sudo apt install ros-melodic-camera-info-manager-py -y
 
 sudo apt-get install v4l-utils -y
