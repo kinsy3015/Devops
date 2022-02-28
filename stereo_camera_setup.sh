ffc16d9998bdb4961d3b8f1045d9443760864ba8
Sleepfuler <62363841+kinsy3015@users.noreply.github.com>
2022-02-28 15:41:37 +0900

    Update stereo_camera_setup.sh

--- stereo_camera_setup.sh
+++ stereo_camera_setup.sh
@@ -2,6 +2,8 @@
 
 sudo pip install v4l2
 
-sudo apt install ros-melodic-camera-info-manager-py
+sudo apt install ros-melodic-camera-info-manager-py -y
+
+sudo apt-get install v4l-utils -y
 
 # preference site : https://youtu.be/wvMkZqv_gnw
