2cf7c4fedf4d320281a7fa92987bedd5840bf4c6
Sleepfuler <62363841+kinsy3015@users.noreply.github.com>
2022-03-15 14:16:41 +0900

    Update Nvidia_driver.sh

--- Nvidia_driver.sh
+++ Nvidia_driver.sh
@@ -21,3 +21,5 @@ echo "Please enter your nvidia-driver-xxx sudo apt-get install"
 
 https://drive.google.com/file/d/19sS1aPK7c6zpP1QTBCObvPFiF1YEVjgn/view?usp=sharing
 echo "Please disable security booting"https://drive.google.com/file/d/19s
+
+#reference site :https://forums.developer.nvidia.com/t/ubuntu-18-04-login-loop-after-installing-nvidia-driver/81280
