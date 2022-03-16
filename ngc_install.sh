d25e6581ddc78ab0cf3be88ac853cb57fd7cfa70
Sleepfuler <62363841+kinsy3015@users.noreply.github.com>
2022-03-16 09:38:26 +0900

    Update ngc_install.sh

--- ngc_install.sh
+++ ngc_install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo ngc API Key Location : nvidia-developer, setup, Generate Key 
 
-
 curl https://get.docker.com | sh  && sudo systemctl start docker && sudo systemctl enable docker
 
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID) && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
