43d487bb3b08594587225d8beee8efdfe774f7a7
Sleepfuler <62363841+kinsy3015@users.noreply.github.com>
2022-03-30 13:16:31 +0900

    Update System_set.sh

--- System_set.sh
+++ System_set.sh
@@ -3,6 +3,9 @@
 
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install vim tmux cmake make gcc ibus-hangul build-essential docker.io -y
+sudo add-apt-repository multiverse
+sudo apt-get update
+sudo apt install ubuntu-restricted-extras -y
 sudo apt autoremove -y
 
 sudo echo -e "set number \ncolo darkblue \nsyntax on" >> ~/.vimrc
