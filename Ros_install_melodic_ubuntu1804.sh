7b5a27a5e73797bba79a911ed0fae5f7aab08985
Sleepfuler <62363841+kinsy3015@users.noreply.github.com>
2022-05-10 18:05:50 +0900

    Update Ros_install_melodic_ubuntu1804.sh

--- Ros_install_melodic_ubuntu1804.sh
+++ Ros_install_melodic_ubuntu1804.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # ROS install on Ubuntu 18.04 Ros melodic
+sudo apt-get update
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
